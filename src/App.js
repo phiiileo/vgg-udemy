4fe1f8b5ba9761b995c305bdd484fe20071c9bc9
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-04-18 15:46:24 +0100

    setup scss file compiler

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ import SignIn from './Components/Auth/SignIn';
 
 function App() {
   return (
+  <div className="App">
     <Router>
       <Switch>
           <Route exact path="/"><SignIn /></Route>
           <Route path="/home"><Home /></Route>
       </Switch>
     </Router>
-    // <div className="App">
-      
-    // </div>
+  </div>
   );
 }
 
--- src/Components/Auth/SignIn.jsx
+++ src/Components/Auth/SignIn.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
-
+import './signin.scss'
 export default class SignIn extends Component {
     render() {
         return (
-            <div>
+            <div className="sign-in">
                 <h1>Sign in here</h1>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="username">Username:</label>
+                        <input type="text" name="username"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" name="password"/>
+                    </div>
+                   <div className="btns">
+                       <button type="submit">Login</button>
+                       <button type="button">Login With Google</button>
+                   </div>
+                </form>
             </div>
         )
     }
