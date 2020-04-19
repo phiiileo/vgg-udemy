46029f9943b0d338a2d16ce1d91e6c41bd3cadb7
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-04-19 22:58:29 +0100

    fix

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import './header.scss'
 
 export default function Header(props) {
+
+    const resetLogin = () => {
+        localStorage.setItem("vgg-user", {})
+        window.location.href = "/"
+    }
     return (
-      <header>
-          <h3>{props.data}</h3>
-         <button>
-             Logout
+        <header>
+            <h3>{props.data}</h3>
+            <button onClick={resetLogin}>
+                Logout
          </button>
-      </header>
+        </header>
     )
 }
 
 Header.defaultProps = {
-    data:"email@email.com"
+    data: "email@email.com",
 }
