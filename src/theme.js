8b7e71651e5f9886952dd021e71022e21483b190
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-08-18 22:18:08 +0100

    color trial

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,15 @@ import {
 } from '@material-ui/core/styles';
 
 
-const theme = createMuiTheme({})
+const theme = createMuiTheme({
+    palette:{
+        primary:{
+            main:"#99223E"
+        },
+        secondary:{
+            main:"#094745"
+        }
+    }
+})
 
 export default theme
