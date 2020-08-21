1ed214091666a0da4f5339e5f48af9b523fe42fc
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-08-21 01:10:07 +0100

    fix

--- src/views/UploadVideo/UploadVideo.jsx
+++ src/views/UploadVideo/UploadVideo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import DashboardLayout from '../../Layout/DashboardLayout'
 import { Typography } from '@material-ui/core'
 import CustomInput from '../../components/customComponents/CustomInput'
@@ -10,13 +10,22 @@ export default function UploadVideo() {
     const [cloudinaryConfig] = useState({
         cloudName: process.env.REACT_APP_CLOUDINARY_NAME,
         unsignedUploadPreset: process.env.REACT_APP_CLOUDINARY_UNSIGNEDUPLOADPRESET,
-        url: "",
         uploadProgress: 0,
         progressBar: "none",
         base_url: ""
     })
     const [file, setFile] = useState()
+    const [progress, setProgress] = useState(0)
+    const [videoInfo, setVideoInfo] = useState({
+        title: "",
+        amount: ""
+    })
 
+    const handleInputChange = (e) => {
+        setVideoInfo({
+            ...videoInfo, [e.target.name]: e.target.value
+        })
+    }
     const getFiles = (e) => {
         console.log(e.target.files);
         const file = e.target.files[0]
@@ -37,8 +46,8 @@ export default function UploadVideo() {
         // Update progress (can be used to show progress indicator)
         http.upload.addEventListener("progress", (progressData) => {
             var progress = Math.round((progressData.loaded * 100) / progressData.total);
-            console.log(progress)
-            console.log(`Amount Uploaded: ${progressData.loaded}, Total Data: ${progressData.total}`);
+            setProgress(progress)
+            // console.log(`Amount Uploaded: ${progressData.loaded}, Total Data: ${progressData.total}`);
         });
 
         http.onreadystatechange = (e) => {
@@ -53,7 +62,7 @@ export default function UploadVideo() {
     }
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(file)
+        console.log(file, videoInfo)
         if (file) {
             uploadFile(file)
         }
@@ -61,15 +70,20 @@ export default function UploadVideo() {
 
     const saveFileToDb = async (videoData) => {
         const payload = {
-            title: "Test upload",
+            title: videoInfo.title,
+            amount: videoInfo.amount,
             url: videoData.secure_url,
             cloud_signature: videoData.signature,
             meta: JSON.stringify(videoData)
         }
 
         const res = await axios.post('/videos', payload);
-        console.log(res)
+        console.log(res.data)
     }
+
+    useEffect(() => {
+        console.log(progress)
+    })
     return (
         <DashboardLayout>
             <section>
@@ -77,8 +91,8 @@ export default function UploadVideo() {
                 <div>
                     <form action="" onSubmit={handleSubmit}>
                         <CustomInput type='file' label='' onChange={getFiles} />
-                        <CustomInput label='Title' />
-                        <CustomInput label='Amount' />
+                        <CustomInput value={""} onChange={handleInputChange} name="title" label='Title' />
+                        <CustomInput label='Amount' onChange={handleInputChange} name="amount" />
                         <CustomButton type='submit' variant='contained' color="primary" button_text='Upload' />
                     </form>
                 </div>
